b336454ea20511b8c768e8398a40e0edee85767b
zengchaowu <root@zengchaowu.com>
2022-02-09 15:48:40 +0800

    chore: 软链换成硬链接，因为HBuilderX识别不了软链。

--- script/setup.sh
+++ script/setup.sh
@@ -1,12 +1,15 @@
 rm -fr .hbuilderx
-ln -s hybird/uniapp/.hbuilderx .hbuilderx
+ln hybird/uniapp/.hbuilderx .hbuilderx
 rm -fr manifest.json
-ln -s hybird/uniapp/manifest.json manifest.json
+ln hybird/uniapp/manifest.json manifest.json
 rm -fr pages.json
-ln -s hybird/uniapp/pages.json pages.json
+ln hybird/uniapp/pages.json pages.json
 rm -fr uni.scss
-ln -s hybird/uniapp/uni.scss uni.scss
+ln hybird/uniapp/uni.scss uni.scss
 rm -fr main.js
-ln -s src/main.js main.js
+ln src/main.js main.js
 rm -fr pages
-ln -s src/pages pages
+ln src/pages pages
+# 给App.vue创建一个硬链接，让HBuilderX识别为uniapp项目
+rm -fr App.vue
+ln src/App.vue App.vue
