95b36a707547381f43d8556c1850f67b7d671ffa
zengchaowu <root@zengchaowu.com>
2022-02-11 14:59:23 +0800

    chore: add layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <Scaffold>
+    <Aside />
+    <main
+      class="absolute inset-y-0 left-52 xl:left-84 right-0 bg-gray-4 overflow-y-auto overflow-x-hidden"
+    >
+      <router-view />
+    </main>
+  </Scaffold>
+</template>
+
+<script>
+import Scaffold from '@/components/layout/Scaffold.vue'
+import Aside from '@/components/layout/Aside.vue'
+export default {
+  components: {
+    Scaffold,
+    Aside
+  }
+}
+</script>
