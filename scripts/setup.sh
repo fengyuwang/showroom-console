0be982ec55010be8cc0874c122c5499a3796d77a
zengchaowu <root@zengchaowu.com>
2022-02-12 13:07:04 +0800

    chore: 按照uniapp官方推荐调整目录结构

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,2 @@
-rm -fr .hbuilderx
-ln -s hybird/uniapp/.hbuilderx .hbuilderx
-rm -fr manifest.json
-ln hybird/uniapp/manifest.json manifest.json
-rm -fr pages.json
-ln hybird/uniapp/pages.json pages.json
-rm -fr uni.scss
-ln hybird/uniapp/uni.scss uni.scss
-rm -fr main.js
-ln src/main.js main.js
-rm -fr pages
-ln -s src/pages pages
-# 给App.vue创建一个硬链接，让HBuilderX识别为uniapp项目
-rm -fr App.vue
-ln src/App.vue App.vue
-
 # 创建文件夹，不然插件可能会写入失败
 mkdir tmp
---
+++ src/pages/index/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-red-50 h-10"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
