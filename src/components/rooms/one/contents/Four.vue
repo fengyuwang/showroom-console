b306bdf33cca95c70ec368215258041d3b85e677
zengchaowu <root@zengchaowu.com>
2022-03-04 09:22:40 +0800

    chore: 调整位置

--- src/components/rooms/one/contents/Four.vue
+++ src/components/rooms/one/contents/Four.vue
@@ -3,10 +3,61 @@
     class="absolute w-2/5 top-4 bottom-10 flex flex-col justify-between bg-background rounded-xl border-primary border-4"
   >
     <div class="flex flex-col mx-8 mt-8 z-10 text-primary text-xl">
-      <div>Global Data</div>
+      <div>Honorary Award</div>
     </div>
-    <div class="flex flex-col items-end mx-8 mb-8 z-10 text-primary text-xl">
-      <div>全球数据</div>
+    <div class="flex flex-col">
+      <view
+        v-for="(row, index) in rows"
+        :key="index"
+        class="flex justify-center align-center p-5 m-2 rounded-lg text-xl"
+        :class="
+          currentIndex == index
+            ? 'bg-primary text-white'
+            : 'bg-white text-primary'
+        "
+        @tap="tap(index)"
+      >
+        {{ row }}
+      </view>
+    </div>
+    <div class="flex flex-col items-end mx-8 mb-8 z-10 text-primary text-3xl">
+      <div>荣誉奖项</div>
     </div>
   </view>
 </template>
+
+<script>
+import { send } from '@/socket/index.js'
+export default {
+  data() {
+    return {
+      rows: [
+        '世界媒体500强',
+        '综合实力企业20强',
+        '湖南省省长质量奖',
+        '超媒文化企业30强',
+        '软件百强企业'
+      ],
+      currentIndex: undefined
+    }
+  },
+  methods: {
+    tap(index) {
+      if (this.currentIndex === index) {
+        return
+      }
+      this.currentIndex = index
+      const data = {
+        type: 'opt',
+        room_id: '1',
+        page_id: 4,
+        operation_id: 0,
+        data: {
+          tex: this.currentIndex + 1
+        }
+      }
+      send(JSON.stringify(data))
+    }
+  }
+}
+</script>
--- src/components/rooms/one/contents/Six.vue
+++ src/components/rooms/one/contents/Six.vue
@@ -3,61 +3,24 @@
     class="absolute w-2/5 top-4 bottom-10 flex flex-col justify-between bg-background rounded-xl border-primary border-4"
   >
     <div class="flex flex-col mx-8 mt-8 z-10 text-primary text-xl">
-      <div>Honorary Award</div>
+      <div>MangoTV</div>
+      <div>Media Constitute</div>
     </div>
-    <div class="flex flex-col">
-      <view
-        v-for="(row, index) in rows"
-        :key="index"
-        class="flex justify-center align-center p-5 m-2 rounded-lg text-xl"
-        :class="
-          currentIndex == index
-            ? 'bg-primary text-white'
-            : 'bg-white text-primary'
-        "
-        @tap="tap(index)"
-      >
-        {{ row }}
-      </view>
+    <div class="flex justify-center item-center">
+      <Control />
     </div>
-    <div class="flex flex-col items-end mx-8 mb-8 z-10 text-primary text-3xl">
-      <div>荣誉奖项</div>
+    <div class="flex flex-col items-end mx-8 mb-8 z-10 text-primary text-xl">
+      <div>芒果</div>
+      <div>超媒构成</div>
     </div>
   </view>
 </template>
 
 <script>
-import { send } from '@/socket/index.js'
+import Control from '@/components/foundation/Control.vue'
 export default {
-  data() {
-    return {
-      rows: [
-        '世界媒体500强',
-        '综合实力企业20强',
-        '湖南省省长质量奖',
-        '超媒文化企业30强',
-        '软件百强企业'
-      ],
-      currentIndex: undefined
-    }
-  },
-  methods: {
-    tap(index) {
-      if (this.currentIndex === index) {
-        return
-      }
-      this.currentIndex = index
-      const data = {
-        type: 'opt',
-        room_id: '1',
-        page_id: 4,
-        operation_id: 0,
-        data: {
-          tex: this.currentIndex + 1
-        }
-      }
-      send(JSON.stringify(data))
-    }
+  components: {
+    Control
   }
 }
 </script>
