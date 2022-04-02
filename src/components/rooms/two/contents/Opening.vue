6ec83e914e9d5b95cc9f689332d785b0c57191bf
zengchaowu <root@zengchaowu.com>
2022-04-02 14:17:33 +0800

    chore: add control

--- src/components/rooms/two/contents/Opening.vue
+++ src/components/rooms/two/contents/Opening.vue
@@ -1,65 +1,18 @@
 <template>
-  <div
-    class="absolute top-4 bottom-10 w-2/5 flex flex-col bg-background rounded-xl border-primary border-4"
+  <view
+    class="absolute w-2/5 top-4 bottom-10 flex flex-col justify-center bg-background rounded-xl border-primary border-4"
   >
-    <div class="flex flex-col mx-8 mt-8 z-10 text-primary">
-      <div class="text-xl">大事记</div>
-      <div>Timeline</div>
+    <div class="flex justify-center item-center overflow-hidden">
+      <Control />
     </div>
-    <div class="mx-8 mt-4 mb-10 overflow-scroll flex flex-wrap">
-      <view
-        v-for="year in years"
-        :key="year"
-        :class="year == current ? 'selected' : 'normal'"
-        @tap="tap(year)"
-      >
-        {{ year }}
-      </view>
-    </div>
-  </div>
+  </view>
 </template>
 
 <script>
-import { send } from '@/socket/index.js'
-import store from '@/store/index.js'
+import Control from '@/components/foundation/Control.vue'
 export default {
-  data() {
-    return {
-      years: [],
-      current: undefined
-    }
-  },
-  created() {
-    const _years = [
-      1949, 1970, 1993, 1996, 1997, 1999, 2000, 2001, 2002, 2004, 2005, 2006,
-      2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
-      2019, 2020, 2021
-    ]
-    this.years = _years.reverse()
-  },
-  methods: {
-    tap(year) {
-      if (this.current === year) return
-      this.current = year
-      const data = {
-        type: 'opt',
-        room_id: store.state.roomId,
-        page_id: store.state.currentItem.tag,
-        operation_id: 0,
-        data: {
-          year: this.current
-        }
-      }
-      send(JSON.stringify(data))
-    }
+  components: {
+    Control
   }
 }
 </script>
-<style scoped>
-.selected {
-  @apply px-4 py-2 mx-3 my-4 rounded-md bg-primary text-white border-gray-50 border-1;
-}
-.normal {
-  @apply px-4 py-2 mx-3 my-4 rounded-md bg-white text-primary border-primary border-1;
-}
-</style>
--- src/components/rooms/two/contents/VIPBusiness.vue
+++ src/components/rooms/two/contents/VIPBusiness.vue
@@ -1,12 +1,18 @@
 <template>
   <view
-    class="absolute w-2/5 top-4 bottom-10 flex flex-col justify-between bg-background rounded-xl border-primary border-4"
+    class="absolute w-2/5 top-4 bottom-10 flex flex-col justify-center bg-background rounded-xl border-primary border-4"
   >
-    <div class="flex flex-col mx-8 mt-8 z-10 text-primary text-xl">
-      <div>Global Data</div>
-    </div>
-    <div class="flex flex-col items-end mx-8 mb-8 z-10 text-primary text-xl">
-      <div>全球数据</div>
+    <div class="flex justify-center item-center overflow-hidden">
+      <Control />
     </div>
   </view>
 </template>
+
+<script>
+import Control from '@/components/foundation/Control.vue'
+export default {
+  components: {
+    Control
+  }
+}
+</script>
