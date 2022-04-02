7fe7d4556b5a13d498d68bef198d47c1d4014762
zengchaowu <root@zengchaowu.com>
2022-04-02 10:06:51 +0800

    chore: layout使用小写文件名

---
+++ src/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="absolute flex inset-0 bg-black">
+    <Aside />
+    <div class="relative flex flex-col flex-grow">
+      <Header />
+      <main
+        class="relative flex-grow bg-gray-4 overflow-y-auto overflow-x-hidden"
+      >
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Aside from '@/components/Aside.vue'
+
+export default {
+  components: {
+    Header,
+    Aside
+  }
+}
+</script>
